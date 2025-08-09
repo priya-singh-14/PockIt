d5ab6ac33dd523b33250dce2589cac7012e7f4ed
priya-singh-14 <singh.priya3@northeastern.edu>
2025-08-09 14:34:01 -0400

    feat: web scrape endpoint

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import axios from "axios";
 
 dotenv.config();
 
@@ -14,4 +15,34 @@ app.get("/api/health", (req, res) => {
   res.json({
     response: "healthy",
   });
+});
+
+app.get("/api/product-details", async (req, res) => {
+  try {
+    const baseURL = process.env.BASE_URL || "";
+    const token = process.env.API_TOKEN;
+
+    const productUrl = req.query.url;
+    if (!productUrl) {
+      return res
+        .status(400)
+        .json({ error: "Missing required query parameter: url" });
+    }
+    const params = {
+      token,
+      url: productUrl,
+    };
+
+    const response = await axios.get(baseURL, {
+      params: params,
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
